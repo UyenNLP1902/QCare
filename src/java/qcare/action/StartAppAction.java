08003628c27be09457468cc93cd6c147dd703926
mtus23 <minhtuz2302@gmail.com>
2020-07-19 09:25:13 +0700

    fis StartAppAction

--- src/java/qcare/action/StartAppAction.java
+++ src/java/qcare/action/StartAppAction.java
@@ -7,7 +7,12 @@ package qcare.action;
 
 import com.opensymphony.xwork2.ActionContext;
 import java.util.Map;
+import qcare.belongs.BelongsDAO;
 import qcare.city.CityDAO;
+import qcare.clinic.ClinicDAO;
+import qcare.comment.CommentDAO;
+import qcare.forum.ForumDAO;
+import qcare.has.HasDAO;
 import qcare.user.UserDAO;
 import qcare.role.RoleDAO;
 import qcare.specialist.SpecialistDAO;
@@ -31,7 +36,11 @@ public class StartAppAction {
         application.put("ROLE_DAO", new RoleDAO());
         application.put("SPECIALIST_DAO", new SpecialistDAO());
         application.put("SYMPTOM_DAO", new SymptomDAO());
-
+        application.put("BELONGS_DAO", new BelongsDAO());
+        application.put("CLINIC_DAO", new ClinicDAO());
+        application.put("COMMENT_DAO",new CommentDAO());
+        application.put("FORUM_DAO",new ForumDAO());
+        application.put("HAS_DAO", new HasDAO());
         return success;
     }
 
