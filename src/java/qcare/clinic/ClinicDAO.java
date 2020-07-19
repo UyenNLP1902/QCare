34cd9afcb35b6c9a38a34a7e70a39da2803c24a0
UyenNLP1902 <nguyenlephuonguyen.1902@gmail.com>
2020-07-19 09:24:04 +0700

    clinic getall

--- src/java/qcare/clinic/ClinicDAO.java
+++ src/java/qcare/clinic/ClinicDAO.java
@@ -10,6 +10,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.NamingException;
+import qcare.util.DBHelper;
 
 /**
  *
@@ -34,5 +39,34 @@ public class ClinicDAO implements Serializable {
         if (con != null) {
             con.close();
         }
+    }
+
+    public List<ClinicDTO> getAll() throws NamingException, SQLException {
+        List<ClinicDTO> result = null;
+        try {
+            String sql = "SELECT * FROM tblClinic";
+            con = DBHelper.makeConnection();
+            stm = con.prepareStatement(sql);
+            rs = stm.executeQuery();
+
+            while (rs.next()) {
+                if (result == null) {
+                    result = new ArrayList<>();
+                }
+                int ID = rs.getInt("ID");
+                String Name = rs.getNString("Name");
+                String Address = rs.getNString("Address");
+                int City = rs.getInt("City");
+                String Phone = rs.getString("Phone");
+                Time OpenTime = rs.getTime("OpenTime");
+                Time CloseTime = rs.getTime("CloseTime");
+
+                ClinicDTO dto = new ClinicDTO(ID, Name, Address, City, Phone, OpenTime, CloseTime);
+                result.add(dto);
+            }
+        } finally {
+            closeConnection();
+        }
+        return result;
     }
 }
